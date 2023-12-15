09bfddd68a6d00043adaedbefd8adfd7e7f00a32
Divya R <kwrdivyaraj100@gmail.com>
2023-12-15 11:42:29 +0530

    Created first request handler for index page

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
 // Templating engine
-app.engine('hbs', exphbs({extname: '.hbs'}));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 
-app.listen(5000, ()=>{
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(5000, () => {
     console.log("Listening on port 5000");
 })
